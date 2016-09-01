7440b1c35fd17d8f659140aa08df24d174e201be
ghostiek <patricktawil1@hotmail.com>
2016-08-31 22:28:52 -0700

    Cookie Party Challenge

--- Hackerrankfun/Program.cs
+++ Hackerrankfun/Program.cs
@@ -1,15 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Hackerrankfun
+using System;
+class Solution
 {
-    class Program
+
+    // https://www.hackerrank.com/contests/w22/challenges/cookie-party
+
+    static void Main(String[] args)
     {
-        static void Main(string[] args)
+        string[] tokens_n = Console.ReadLine().Split(' ');
+        // Guests
+        int n = Convert.ToInt32(tokens_n[0]);
+        // Cookies made
+        int m = Convert.ToInt32(tokens_n[1]);
+
+        int min = 0;
+        //Everyone needs to have the same amount
+        while (m % n != 0)
         {
+            ++min;
+            ++m;
         }
+        Console.WriteLine(min);
     }
 }
